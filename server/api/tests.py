40b6f003f2d3ba4d23b4e189c5319f95e7dd32ca
IvanSeagull <ivan.seagull.k@gmail.com>
2025-01-16 05:37:15 +0000

    tests

--- server/api/tests.py
+++ server/api/tests.py
@@ -3,6 +3,11 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+from api.models import FriendRequest, Friend
+
+CustomUser = get_user_model()
+
+
 class AuthTests(TestCase):
     def setUp(self):
         self.client = Client()
@@ -114,3 +119,82 @@ class AuthTests(TestCase):
         )
         user = response.wsgi_request.user
         self.assertFalse(user.is_authenticated)
+
+
+class FriendFunctionalityTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+
+        self.user1 = CustomUser.objects.create_user(username='user1', email="email1@gmail.com", password='password1', name='User One')
+        self.user2 = CustomUser.objects.create_user(username='user2', email="email2@gmail.com", password='password2', name='User Two')
+        self.user3 = CustomUser.objects.create_user(username='user3', email="email3@gmail.com", password='password3', name='User Three')
+
+        self.client.login(username='user1', password='password1')
+
+    def test_send_friend_request(self):
+        response = self.client.post(
+            reverse('send_friend_request'),
+            data={'receiver_id': self.user2.id},
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(FriendRequest.objects.count(), 1)
+        self.assertEqual(FriendRequest.objects.first().receiver, self.user2)
+
+    def test_send_friend_request_to_self(self):
+        response = self.client.post(
+            reverse('send_friend_request'),
+            data={'receiver_id': self.user1.id},
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+
+    def test_send_duplicate_friend_request(self):
+        FriendRequest.objects.create(sender=self.user1, receiver=self.user2)
+        response = self.client.post(
+            reverse('send_friend_request'),
+            data={'receiver_id': self.user2.id},
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Friend request already sent', response.json()['error'])
+
+    def test_get_friends(self):
+        Friend.objects.create(first_user=self.user1, second_user=self.user2)
+        response = self.client.get(reverse('get_friends') + f'?username={self.user1.username}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()['friends']), 1)
+        self.assertEqual(response.json()['friends'][0]['username'], self.user2.username)
+
+    def test_get_friend_requests(self):
+        FriendRequest.objects.create(sender=self.user2, receiver=self.user1)
+        response = self.client.get(reverse('get_friend_requests'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()['requests']), 1)
+        self.assertEqual(response.json()['requests'][0]['sender_username'], self.user2.username)
+
+    def test_accept_friend_request(self):
+        friend_request = FriendRequest.objects.create(sender=self.user2, receiver=self.user1)
+        response = self.client.post(reverse('accept_friend_request', args=[friend_request.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Friend.objects.count(), 1)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+
+    def test_reject_friend_request(self):
+        friend_request = FriendRequest.objects.create(sender=self.user2, receiver=self.user1)
+        response = self.client.post(reverse('reject_friend_request', args=[friend_request.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Friend.objects.count(), 0)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+
+    def test_accept_non_existing_friend_request(self):
+        response = self.client.post(reverse('accept_friend_request', args=[999]))
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('Friend request not found', response.json()['error'])
+
+    def test_reject_non_existing_friend_request(self):
+        response = self.client.post(reverse('reject_friend_request', args=[999]))
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('Friend request not found', response.json()['error'])
